1e686de6dcdf097f9b6e823226da6b2ef9de3e80
andrestrw <133chicagobulls@gmail.com>
2024-03-06 18:06:18 +0100

    Recover validated password from backend

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ import bcryt from "bcrypt";
 const router = express.Router();
 import { User } from "../models/User.js";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 router.post("/signUp", async (req, res) => {
   const { username, email, password } = req.body;
@@ -36,6 +37,59 @@ router.post("/login", async (req, res) => {
   });
   res.cookie("token", token, { httpOnly: true, maxAge: 360000 });
   return res.json({ status: true, message: "login successfully" });
+});
+
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "user not registered" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.KEY, {
+      expiresIn: "5m",
+    });
+
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "jandres33110@gmail.com",
+        pass: "ptfi trcw bhxq hurx",
+      },
+    });
+    const encodedToken = encodeURIComponent(token).replace(/\./g, "%2E");
+    var mailOptions = {
+      from: "jandres33110@gmail.com",
+      to: "133chicagobulls@gmail.com",
+      subject: "Reset Password",
+      text: `http://localhost:5173/resetPassword/${encodedToken}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return res.json({ message: "error sending email" });
+      } else {
+        return res.json({ status: true, message: "email sent" });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/reset-password/:token", async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  try {
+    const decoded = await jwt.verify(token, process.env.KEY);
+    const id = decoded.id;
+    const hashPassword = await bcryt.hash(password, 10);
+    await User.findByIdAndUpdate({ _id: id }, { password: hashPassword });
+    return res.json({ status: true, message: "updated password" });
+  } catch (err) {
+    return res.json("invalid token");
+  }
 });
 
 export { router as UserRouter };
